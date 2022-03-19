b851d4509f6df6995932f577191fefd853d0aeda
KomlaRD <ankueric1@gmail.com>
2022-03-19 19:44:56 +0000

    survey questions in project one

---
+++ survey_questions_project_one.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+class_scores <- data.frame(a = c(8,6,5,12,5,6,8,9,7,6,9,5,14,4,7,6))
+
+mean(df12$a)
+
+median(df12$a)
+
+ggplot(df12, aes(a)) +
+        geom_histogram(binwidth = 1)
